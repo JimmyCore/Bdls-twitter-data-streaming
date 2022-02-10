f96bf1a359b5860aba8aa3830cfede9ee8c68df9
JimmyCore <81090710+JimmyCore@users.noreply.github.com>
2022-02-10 16:13:29 +0100

    Add files via upload

--- filtered_stream.py
+++ filtered_stream.py
@@ -1,12 +1,55 @@
+import sys
 import requests
 import os
 import json
+import re
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
+
+
 os.environ[
     'BEARER_TOKEN'] = 'AAAAAAAAAAAAAAAAAAAAAANOZAEAAAAAKqQq4bmvVFjmvPcpmW95CCXFefI%3DjwEXvt6vXAalbdYcBB7bUI52dAyvtDCo2CDBgLVn34sc86o8rk'
 bearer_token = os.environ.get("BEARER_TOKEN")
+
+
+def remove_whitespaces(twt):
+    pattern = re.compile(r'\s+')
+    return re.sub(pattern, '', twt)
+
+
+def remove_emojis(data):
+    emoj = re.compile("["
+                      u"\U0001F600-\U0001F64F"  # emoticons
+                      u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                      u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                      u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                      u"\U00002500-\U00002BEF"  # chinese char
+                      u"\U00002702-\U000027B0"
+                      u"\U00002702-\U000027B0"
+                      u"\U000024C2-\U0001F251"
+                      u"\U0001f926-\U0001f937"
+                      u"\U00010000-\U0010ffff"
+                      u"\u2640-\u2642"
+                      u"\u2600-\u2B55"
+                      u"\u200d"
+                      u"\u23cf"
+                      u"\u23e9"
+                      u"\u231a"
+                      u"\ufe0f"  # dingbats
+                      u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+
+
+def clean_tweet(twt):
+    twt = twt.lower()
+    twt = re.sub('#bitcoin', 'bitcoin', twt)
+    twt = re.sub('#[A-Za-z0-9]+', '', twt)
+    twt = re.sub('\\n', '', twt)
+    twt = re.sub('https?:\/\/\S', '', twt)
+    twt = twt.strip()
+    return twt
 
 
 def bearer_oauth(r):
@@ -54,7 +97,7 @@ def delete_all_rules(rules):
 def set_rules(delete):
     # You can adjust the rules if needed
     sample_rules = [
-        {"value": "#bitcoin", "tag": "#bitcoin"}
+        {"value": "#bitcoin lang:en", "tag": "#bitcoin"}
     ]
     payload = {"add": sample_rules}
     response = requests.post(
@@ -83,7 +126,11 @@ def get_stream(set):
     for response_line in response.iter_lines():
         if response_line:
             json_response = json.loads(response_line)
-            print(json.dumps(json_response, indent=4, sort_keys=True))
+            json_response = json_response['data']
+            json_response['text'] = clean_tweet(json_response['text'])
+            json_response['text'] = remove_emojis(json_response['text'])
+            json_obj = json.dumps(json_response, indent=4, sort_keys=True)
+            sys.stdout.write(json_obj)
 
 
 def main():
